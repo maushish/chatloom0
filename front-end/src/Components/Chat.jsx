de16ded6cceee9e26e5e9d2ea5d1c562954639db
maushish <chamarnumber102@gmail.com>
2023-09-08 23:06:33 +0530

    Changed the logos and introduced profile section

--- front-end/src/Components/Chat.jsx
+++ front-end/src/Components/Chat.jsx
@@ -1,40 +1,42 @@
-import React from 'react'
-import { useLocation } from 'react-router-dom';
+import React from 'react';
+import convo from '../Logo/chatting.svg'
+import pfp from '../Logo/pfp.svg'
 
 function Chat() {
-  const location = useLocation();
-  const avatar = new URLSearchParams(location.search).get('avatar');
-
-
   return (
-    <div className='bg-mainBlack h-screen flex flex-col items-center justify center '>
-      {/*whole chat thing*/}
+    <div className='bg-mainBlack h-screen flex flex-col items-center justify-center'>
+      {/* Whole chat thing */}
       <div className='rounded-xl border border-gray-300 opacity-25 bg-gradient-to-r from-opacity-80 to-opacity-85 from-black to-black flex flex-col items-center justify-center w-[60%] ml-[38%] mt-[3%] h-[92%]'>
         <div>Name</div>
         <div>Chat</div>
         <div>emoji, typing</div>
-
-
       </div>
-      {/*Profiles Section */}
+      {/* Profiles Section */}
       <div className='rounded-xl border border-gray-300 opacity-25 bg-gradient-to-r from-opacity-80 to-opacity-85 from-black to-black flex flex-col items-center justify-center w-[30%] mr-[60%] h-[50%] mt-[-54%] xl:mt-[-35%]'>
-        {/*USER'S PROFILE */}
+        {/* USER'S PROFILE */}
         <div>
-          {avatar ? (
-            <img src={avatar} alt="Avatar" />
-          ) : (
-            <div>No Avatar</div>
-          )}
+          <div className='w-[40vh] h-[8vh] bg-customGreen rounded-lg mt-[-100%]'>
+            <div></div>
+            <div className='z=10 opacity-100'>
+              <a>
+                <img src={convo} className='h-[8vh] ml-20'></img>
+              </a>
+            </div>
+            <div>
+            <a>
+                <img src={pfp} className='h-[6vh] ml-60 mt-[-17%]'></img>
+              </a>
+            </div>
+
+          </div>
+          
         </div>
       </div>
       <div>
-
-        {/*All profiles  */}
+        {/* All profiles */}
       </div>
-
     </div>
-
-  )
+  );
 }
 
-export default Chat
+export default Chat;
